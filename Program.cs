7c13d03ae4c0f01cf3d90bf55f264bb6e08871f4
tlsaad <121204003+tlsaad@users.noreply.github.com>
2022-12-23 16:47:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Prime numbers program ");
+        Console.WriteLine("Enter first number : ");
+        int start = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter last number : ");
+        int end = int.Parse(Console.ReadLine());
+        int total;
+        for (int i = start; i < end; i++)
+        {
+            total = 0;
+            for (int k = 1; k < i; k++)
+            {
+                if (i % k == 0) 
+                {
+                    total = total + k;
+                                                          
+
+                }
+            }
+            if (total == i)
+            {
+                Console.WriteLine(total);
+            }
+
+        }
+        Console.ReadKey();
+    }
+}
